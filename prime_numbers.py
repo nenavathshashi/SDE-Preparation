e4a4f0bc84ae114cad310b28f43ae2c35cdef98d
nenavathshashi <77627671+nenavathshashi@users.noreply.github.com>
2022-12-05 00:45:33 +0530

    between_primes function
    updated a new function which prints all the prime numbers in between two given numbers

--- prime_numbers.py
+++ prime_numbers.py
@@ -14,4 +14,23 @@ class prime:
 			print(True)
 		else:
 			print(False)
+	
+	# function to calculate prime numbers in between given range
+	#Enter the two input in which we have to find the prime numbers
+	def between_range(self,a,b):
+		self.a=a
+		self.b=b
+		checkprime=[0]*(10**5+1)
+		for i in range(b+1):
+			checkprime[i]=1
+		#print(checkprime)
+		for i in range(2,10**5+1):
+			if checkprime[i]==1:
+				for j in range(2,10**5+1):
+					if i*j<10**5:
+						checkprime[i*j]=0
+		#print(checkprime)
+		for i in range(a,b+1):
+			if checkprime[i]==1:
+				print(i,end=" ")
 p=prime(int(input()))
