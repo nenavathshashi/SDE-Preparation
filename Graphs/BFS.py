5059e20ae90ead38db40172c67cfe4198e4cdc34
nenavathshashi <77627671+nenavathshashi@users.noreply.github.com>
2023-03-03 01:21:06 +0530

    Added few more functions
    added functions like calculating connected components,converting adjacency matrix to defaultdict(list)

--- Graphs/BFS.py
+++ Graphs/BFS.py
@@ -7,12 +7,25 @@ class Graph:
         self.graph[u].append(v)
     def display(self):
         return self.graph
-    def BfsRecursive(self,node,visited):
+    def DfsRecursive(self,node,visited):
         visited.add(node)
-        print(node)
+        print(node,end=" ")
         for neighbour in self.graph[node]:
             if neighbour not in visited:
-                self.BfsRecursive(neighbour,visited)
+                self.DfsRecursive(neighbour,visited)
+    def DfsIterative(self,node):
+        stack=deque()
+        stack.append(node)
+        visited=set()
+        while(stack):
+            vertex=stack.pop()
+            if vertex in visited:
+                continue
+            print(vertex,end=" ")
+            visited.add(vertex)
+            for neighbour in self.graph[vertex]:
+                stack.append(neighbour)
+
     def BfsIterative(self,node):
         q=deque()
         q.append(node)
@@ -21,22 +34,59 @@ class Graph:
             vertex=q.popleft()
             if vertex in visited:
                 continue
-            print(vertex)
+            print(vertex,end=" ")
             visited.add(vertex)
             for neighbour in self.graph[vertex]:
                 q.append(neighbour)
+    def BfsRecursive(self,q,visited):
+        #print("entered",len(q),q)
+        if len(q)==0:
+            return
+        vertex=q.popleft()
+        if vertex not in visited:
+            print(vertex,end=" ")
+            visited.add(vertex)
+            for neighbour in self.graph[vertex]:
+                q.append(neighbour)
+            self.BfsRecursive(q,visited)
+
     def adj_to_dict(self,adj):
         for i in range(len(adj)):
             for j in range(len(adj[0])):
                 if adj[i][j]==1:
                     self.graph[i].append(j)
         return self.graph
+    def connectedComponents(self,vertices,visited):
+        count=0
+        for i in range(vertices):
+            if i not in visited:
+                visited.add(i)
+                count+=1
+                self.DfsRecursive(i,visited)
+                print()
+        return count
 g=Graph()
-#g.addEdge(0,1)
-#g.addEdge(0,2)
-##g.addEdge(0,3)
-##g.addEdge(2,4)
-##g.addEdge(3,3)
-print(g.adj_to_dict([[0,1,1],[0,0,0],[0,0,0]]))
-g.BfsRecursive(0,set())
+#total vertices
+V=10
+g.addEdge(0,1)
+g.addEdge(0,2)
+g.addEdge(0,3)
+g.addEdge(1,7)
+g.addEdge(7,8)
+g.addEdge(2,4)
+g.addEdge(5,6)
+g.addEdge(9,9)
+print(g.display())
+#print(g.adj_to_dict([[0,1,1],[0,0,0],[0,0,0],]))
+print("DFS in a Recursive approach:")
+g.DfsRecursive(0,set())
+print("\nDFS in a Iterative approach:")
+g.DfsIterative(0)
+print("\nBFS in a Iterative approach:")
 g.BfsIterative(0)
+q=deque()
+q.append(0)
+print("\nBFS in a Recursive approach:")
+g.BfsRecursive(q,set())
+print("\nDifferent connected components")
+print("Total connected components:",g.connectedComponents(V,set()))
